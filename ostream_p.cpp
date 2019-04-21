7ee510dca9151fd5fff8caed1e98c2a858463dfb
David Seungjin Lee <41690461+davidseungjin@users.noreply.github.com>
2019-04-21 14:37:04 -0700

    Update ostream_p.cpp
    I made it for myself: why ostream operator << overloading is necessary: simply speaking, it is necessary when I need to access to private member data of class from outside of class.

--- ostream_p.cpp
+++ ostream_p.cpp
@@ -1,10 +1,22 @@
 // ostream& practice.
-/* Output streams use the insertion << operator for standard types. You can also overload the << operator for your own classes.
-
-The write function example showed the use of a Data structure. A date is an ideal candidate for a C++ class in which the data members (month, day, and year) are hidden from view. An output stream is the logical destination for displaying such as structure. 
-
-To get cout to accept a Data object after the insertion operator, overloaded insertion operator to recognize an ostream object on the left and a Data on the right. The overloaded << operator function must then be declared as a friend of class Date so it can access the private data with a Data object. 
+/* Output streams use the insertion << operator for standard types. 
+You can also overload the << operator for your own classes.
+The write function example showed the use of a Data structure. 
+A date is an ideal candidate for a C++ class in which the data members (month, day, and year) are hidden from view.
+An output stream is the logical destination for displaying such as structure. 
+To get cout to accept a Data object after the insertion operator, 
+overloaded insertion operator to recognize an ostream object on the left and a Data on the right.
+The overloaded << operator function must then be declared as a friend of class Date so it can access the private data with a Data object. 
 */
+
+/* David interpretation. 
+ex1) when public function(constructor or other function) call privite member in calss: no problem.
+ex2) when function outside of class try to access to public memeber data: no problem.
+ex3) when function outside of calss try to access to private member data: error
+ex4) together with friend, when function outside of class try to access to private member data: possible.
+*/
+
+// EXAMPLE 1
 
 #include<iostream>
 using namespace std;
@@ -20,22 +32,85 @@ public:
 		mo=m; da=d; yr=y;
 		cout<< mo << '/' << da << '/' << yr << "\n";
 	}
-//	friend ostream& operator<<(ostream& os, const Data& dt);
 };
 
-/*
+int main()
+{
+	Data dt(5,6,92);
+}	
+
+// EXAMPLE 2
+#include<iostream>
+using namespace std;
+
+class Data
+{ 
+public: 
+	int mo, da, yr;
+public:
+	Data(int m, int d, int y)
+	{
+		mo=m; da=d; yr=y;
+	}
+};
+
+int main()
+{
+	Data dt(5,6,92);
+	cout << dt.mo << '/' << dt.da << '/' << dt.yr << "\n";
+}	
+
+// EXAMPLE 3
+#include<iostream>
+using namespace std;
+
+class Data
+{ 
+
+private:
+	int mo, da, yr;
+public:
+	Data(int m, int d, int y)
+	{
+		mo=m; da=d; yr=y;
+	}
+};
+
+int main()
+{
+	Data dt(5,6,92);
+	cout << dt.mo << '/' << dt.da << '/' << dt.yr << "\n";
+}	
+
+
+// EXAMPLE 4
+#include<iostream>
+using namespace std;
+
+class Data
+{ 
+
+private:
+	int mo, da, yr;
+public:
+	Data(int m, int d, int y)
+	{
+		mo=m; da=d; yr=y;
+	}
+	friend ostream& operator<<(ostream& os, const Data& dt);
+};
+
 ostream& operator<<(ostream& os, const Data& dt)
 {
 	os << dt.mo << '/' << dt.da << '/' << dt.yr;
 	return os;
 }
-*/
-
 
 int main()
 {
 	Data dt(5,6,92);
-//	cout << dt.mo << '/' << dt.da << '/' << dt.yr << "\n";
-
-//	cout << dt << "\n";
+	cout << dt << "\n";
 }	
+
+
+
