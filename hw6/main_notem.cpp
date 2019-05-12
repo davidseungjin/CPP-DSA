9bba43b32f5bbf6facd71f2d969f85a5b4eded20
davidseungjin <ryonlee@gmail.com>
2019-05-11 22:31:07 -0700

    hw6

--- hw6/main_notem.cpp
+++ hw6/main_notem.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <fstream>
+#include "Timer.h"
 
 using namespace std;
 
@@ -14,28 +17,36 @@ public:
     // Constructor
     TreeNode(string k, int e, TreeNode* l, TreeNode* r)
     : key(k), info(e), left(l), right(r){}
-    TreeNode* newNode(string k, int e, TreeNode* l, TreeNode* r){
+    static TreeNode* newNode(string k, int e, TreeNode* l, TreeNode* r){
         return new TreeNode(k, e, l, r);
     }
-    TreeNode* insert(string key, int info, TreeNode* RootT){
-        if (RootT != NULL)
-            RootT = newNode(key, 1, nullptr, nullptr);
+    
+    static TreeNode* insert(string key, int info, TreeNode* RootT){
+        if (RootT != NULL){
+            cout << "1.     test insert" << endl;
+            RootT = newNode(key, info, nullptr, nullptr);
             TreeNode* t = RootT;
-        while (t->key !=  key){
-            if (key < t->key){
-                if (t->left != NULL)
-                    t->left = newNode(key, info, NULL, NULL);
-                    t = t->left;
-            }else if(key > t->key){
-                if (t->right != NULL)
-                    t->right = newNode(key, info, NULL, NULL);
-                    t = t->right;
+            while (t->key !=  key){
+                if (key < t->key){
+                    if (t->left != NULL)
+                        t->left = newNode(key, info, NULL, NULL);
+                        t = t->left;
+                }else if(key > t->key){
+                    if (t->right != NULL)
+                        t->right = newNode(key, info, NULL, NULL);
+                        t = t->right;
+                }
             }
-        }
         t->info = info;
+
+        cout << t -> info << "  t -> info" << endl;
+        cout << t -> key << "   t -> key" << endl;
+
         return RootT;
+        }
     }
-    TreeNode* find(string key , TreeNode* t){
+    
+    static TreeNode* find(string key , TreeNode* t){
         while (t != nullptr){
             if ( key  <  t->key )
                 t = t->left;
@@ -46,7 +57,7 @@ public:
         }
         return nullptr;
     }
-    TreeNode* remove(string key , TreeNode* t){
+    static TreeNode* remove(string key , TreeNode* t){
         TreeNode* toRemove = find(key, t); // must do in-line, assn
         if (toRemove -> left  ==  nullptr){
             if ( toRemove -> right  ==  nullptr )
@@ -56,101 +67,46 @@ public:
         }else if( toRemove -> right == nullptr ){
             return toRemove->left;
         }else{  // hard case, two children
-            TreeNode* predecessorNode = findPred(key, toRemove);
-            swapKeyAndInfo(predecessorNode, toRemove);
-            toRemove = remove(key, toRemove);
+//          Need to look at and implement!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//           TreeNode* predecessorNode = findPred(key, toRemove);
+//         swapKeyAndInfo(predecessorNode, toRemove);
+//           toRemove = remove(key, toRemove);
             return toRemove;
         }
     }
-    void print(ostream & out , TreeNode * t);
-    void deleteNode(TreeNode* t) { delete t; }
-    void deleteTree(TreeNode* t);
+    static void print(ostream &out , TreeNode* t){
+        if ( t ){
+            out << '[';
+            print(out, t->left);
+            out << '(' << t->key << ',' << t->info << ')';
+            print(out, t->right);
+            out << ']';
+        }else
+            out << "nullptr";
+    }
+    
+//    void deleteNode(TreeNode* t) { delete t; }            delete this statement
+    static void deleteTree(TreeNode* t);
 };
 
-//
-//TreeNode::TreeNode* newNode(string k, int e,TreeNode * l, TreeNode * r)
-//{
-//    ++nodesCreated;
-//    return new TreeNode(k, e, l, r);
-//}
-//
-//TreeNode::insert(string key, int info, TreeNode *RootT ){
-//    if (RootT != NULL)
-//        RootT = newNode(key, info, nullptr, nullptr);
-//    TreeNode* t = RootT;
-//    while (t->key !=  key){
-//        if (key < t->key){
-//            if (t->left != NULL)
-//                t->left = newNode(key, info, NULL, NULL);
-//            t = t->left;
-//        }else if(key > t->key){
-//            if (t->right != NULL)
-//                t->right = newNode(key, info, NULL, NULL);
-//            t = t->right;
-//        }
-//    }
-//    t->info = info;
-//    return RootT;
-//}
-
-//TreeNode::find(string key, TreeNode* t){
-//    while (t != nullptr){
-//        if ( key  <  t->key )
-//            t = t->left;
-//        else if ( key  >  t->key )
-//            t = t->right;
-//        else // found it!
-//            return t;
-//    }
-//    return nullptr;
-//}
-//
-//TreeNode::remove( string key, TreeNode* t )
-//{
-//    TreeNode* toRemove = find(key, t); // must do in-line, assn
-//    if (toRemove -> left  ==  nullptr){
-//        if ( toRemove -> right  ==  nullptr )
-//            return NULL;
-//        else
-//            return toRemove -> right;
-//    }else if( toRemove -> right == nullptr ){
-//            return toRemove->left;
-//    }else{  // hard case, two children
-//        TreeNode * predecessorNode = findPred(key, toRemove);
-//        swapKeyAndInfo(predecessorNode, toRemove);
-//        toRemove = remove(key, toRemove);
-//        return toRemove;
-//    }
-//}
-
-void TreeNode::print(ostream & out, TreeNode* t)
-{
-    if ( t ){
-        out << '[';
-                 print(out, t->left);
-                 out << '(' << t->key << ',' << t->info << ')';
-                 print(out, t->right);
-                 out << ']';
-    }else
-        out << "nullptr";
-}
-
+/*
 static void deleteNode(TreeNode* t){
     --nodesCreated;
     delete t;
 }
-        
+*/
+
 
 static void deleteTree(TreeNode* t){
     if ( t ){
         deleteTree(t->left);
         deleteTree(t->right);
-        deleteNode(t);
+        delete t;            //deleteNode(t); <--- I don't think it is necessary.
     }
 }
 
 class BinarySearchTree{
-    TreeNode * root;
+    TreeNode* root;
 public:
     void insert(string key, int info);
     int find(string key);
@@ -163,41 +119,69 @@ void BinarySearchTree::insert(string key, int info){
 }
         
 int BinarySearchTree::find(string key){
+    cout << "1.             find function of BST class" << endl;
     TreeNode* t = TreeNode::find(key, root);
+
+/*
     if ( !t ){
         insert(key, int());
         t = TreeNode::find(key, root);
     }
+*/
+    cout << "2.             find function of BST class" << endl;
     return t->info;
 }
 
 void BinarySearchTree::remove(string key){
     root = TreeNode::remove(key, root);
 }
-        
+
+
 void BinarySearchTree::print(ostream& out){
     TreeNode::print(out, root);
+    
 }
 
         
-void insertAllWords(BinarySearchTree& T, int partition, char *fileName){
+void insertAllWords(BinarySearchTree& T, int partition, string& input_file){
     Timer t;
     double eTime;
     ifstream f(input_file.c_str());
     t.start();
     string w;
     if(f.is_open()){
-//        while(getline(f, w)){
-        for(int i=0; partition*4500; i++){
-            L.insert(w, 1);               // test test test test
-        }
+        while(getline(f, w)){
+//        for(int i=0; i < partition*10; i++){
+//            cout << i << " is counting partition" << endl;
+            cout << "w is " << w << endl;
+            T.insert(w, 1);               // test test test test
+//        }
     }
+    }
+    
     t.elapsedUserTime(eTime);
     f.close();
     cout << "insertAllWords time of\t" << "partition\t" << partition << " is " << eTime << endl;
 }
 
-void findAllWords(BinarySearchTree & T, int partition, char *fileName){
+void findAllWords(BinarySearchTree& T, int partition, string& input_file){
+    Timer t;
+    double eTime;
+    ifstream f(input_file.c_str());
+    t.start();
+    string w;
+    if(f.is_open()){
+        while(getline(f, w)){
+//        for(int i=0; i < partition*4500; i++){
+            T.find(w);               // test test test test
+        }
+    }
+    t.elapsedUserTime(eTime);
+    f.close();
+    cout << "findAllWords time of\t" << "partition\t" << partition << " is " << eTime << endl;
+}
+        
+void removeAllWords(BinarySearchTree& T, int partition, string& input_file){
     Timer t;
     double eTime;
     ifstream f(input_file.c_str());
@@ -205,38 +189,31 @@ void findAllWords(BinarySearchTree & T, int partition, char *fileName){
     string w;
     if(f.is_open()){
 //        while(getline(f, w)){
-        for(int i=0; partition*4500; i++){
-            L.find(w);               // test test test test
+        for(int i=0; i < partition*4500; i++){
+            T.remove(w);               // test test test test
         }
     }
+    
     t.elapsedUserTime(eTime);
     f.close();
-    cout << "insertAllWords time of\t" << "partition\t" << partition << " is " << eTime << endl;
+    cout << "removeAllWords time of\t" << "partition\t" << partition << " is " << eTime << endl;
 }
         
-void removeAllWords(BinarySearchTree & T, int partition, char *fileName){
-    Timer t;
-    double eTime;
-    ifstream f(input_file.c_str());
-    t.start();
-    string w;
-    if(f.is_open()){
-//        while(getline(f, w)){
-        for(int i=0; partition*4500; i++){
-            L.remove(w);               // test test test test
-        }
-    }
-    t.elapsedUserTime(eTime);
-    f.close();
-    cout << "insertAllWords time of\t" << "partition\t" << partition << " is " << eTime << endl;
-}
+void measureAll(string input_file, BinarySearchTree& T){
+    for (int i=1; i<=1; ++i){
+        BinarySearchTree T;             //important to start with an empty Tree here
+        insertAllWords(T, i, input_file);
         
-void measureAll(char *fileName, BinarySearchTree& T){
-    for (int i=1; i<=10; ++i){
-//       BinarySearchTree T; important to start with an empty Tree here
-        insertAllWords(T, i, fileName);
-        findAllWords(T, i, fileName);
-        removeAllWords(T, i, fileName);
+        
+        
+        
+//        T.print(ostream& out);
+        
+        
+        
+        findAllWords(T, i, input_file);
+//        removeAllWords(T, i, input_file);
+        cout << "test" << endl;
     }
 }
         
@@ -244,7 +221,7 @@ int main(int argc, char* argv[]){
             
     const char* input_file = argc == 2? argv[1] : "random_5.txt";
 
-    TreeNode D;
+    BinarySearchTree D;
     
     measureAll(input_file, D);
 
