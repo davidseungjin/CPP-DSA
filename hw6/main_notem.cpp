1bcb69723dc2c2a63808c7fe4f8fb1ed6373f6e1
davidseungjin <ryonlee@gmail.com>
2019-05-11 20:26:18 -0700

    hw6

--- hw6/main_notem.cpp
+++ hw6/main_notem.cpp
@@ -3,156 +3,184 @@
 
 using namespace std;
 
-template <typename KeyType, typename ElementType, typename TreeNode>
 class TreeNode{
-    KeyType key;
-    ElementType info;
+public:
+    string key;
+    int info;
     TreeNode* left;
     TreeNode* right;
+    
 public:
-    TreeNode(KeyType newKey, ElementType newInfo,
-             TreeNode *l, TreeNode *r);
-    static TreeNode* newNode(KeyType k, ElementType e,
-                              TreeNode * l, TreeNode * r) { return new TreeNode(k,e,l); }
-    static TreeNode* insert(KeyType key, ElementType info, TreeNode * t);
-    static TreeNode* find(KeyType key , TreeNode * t);
-    static TreeNode* remove(KeyType key , TreeNode * t);
-    static void print(ostream & out , TreeNode * t);
-    static void deleteNode(TreeNode* t) { delete t; }
-    static void deleteTree(TreeNode* t);
-};
-
-
-// Constructor
-template <typename KeyType, typename ElementType, typename TreeNode>
-TreeNode(KeyType k, ElementType e,TreeNode *l, TreeNode *r)‏
-: key(k), info(e), left(l), right(r)‏{}
-
-template <typename KeyType, typename ElementType, typename TreeNode>
-TreeNode* newNode(KeyType k, ElementType e,TreeNode * l, TreeNode * r)‏
-{
-    ++nodesCreated;
-    return new TreeNode(k, e, l, r);
-}
-
-template <typename KeyType, typename ElementType, typename TreeNode>
-TreeNode* insert(KeyType key, InfoType info, TreeNode *RootT ){
-    if (RootT != NULL)
-        RootT = newNode(key, info, nullptr, nullptr);
-    TreeNode* t = RootT;
-    while (t->key !=  key){ ‏
-        if (key < t->key)‏{
-            if (t->left != NULL)‏
-                t->left = newNode(key, info, NULL, NULL);
-            t = t->left;
-        }else if(key > t->key)‏ {
-            if (t->right != NULL)‏
-                t->right = newNode(key, info, NULL, NULL);
-            t = t->right;
+    // Constructor
+    TreeNode(string k, int e, TreeNode* l, TreeNode* r)
+    : key(k), info(e), left(l), right(r){}
+    TreeNode* newNode(string k, int e, TreeNode* l, TreeNode* r){
+        return new TreeNode(k, e, l, r);
+    }
+    TreeNode* insert(string key, int info, TreeNode* RootT){
+        if (RootT != NULL)
+            RootT = newNode(key, 1, nullptr, nullptr);
+            TreeNode* t = RootT;
+        while (t->key !=  key){
+            if (key < t->key){
+                if (t->left != NULL)
+                    t->left = newNode(key, info, NULL, NULL);
+                    t = t->left;
+            }else if(key > t->key){
+                if (t->right != NULL)
+                    t->right = newNode(key, info, NULL, NULL);
+                    t = t->right;
+            }
+        }
+        t->info = info;
+        return RootT;
+    }
+    TreeNode* find(string key , TreeNode* t){
+        while (t != nullptr){
+            if ( key  <  t->key )
+                t = t->left;
+            else if ( key  >  t->key )
+                t = t->right;
+            else // found it!
+                return t;
+        }
+        return nullptr;
+    }
+    TreeNode* remove(string key , TreeNode* t){
+        TreeNode* toRemove = find(key, t); // must do in-line, assn
+        if (toRemove -> left  ==  nullptr){
+            if ( toRemove -> right  ==  nullptr )
+                return NULL;
+            else
+                return toRemove -> right;
+        }else if( toRemove -> right == nullptr ){
+            return toRemove->left;
+        }else{  // hard case, two children
+            TreeNode* predecessorNode = findPred(key, toRemove);
+            swapKeyAndInfo(predecessorNode, toRemove);
+            toRemove = remove(key, toRemove);
+            return toRemove;
         }
     }
-    t->info = info;
-    return RootT;
-}
+    void print(ostream & out , TreeNode * t);
+    void deleteNode(TreeNode* t) { delete t; }
+    void deleteTree(TreeNode* t);
+};
 
-template <typename KeyType, typename ElementType, typename TreeNode>
-TreeNode* find(KeyType key, TreeNode * t)‏{
-    while (t != nullptr){
-        if ( key  <  t->key )‏
-            t = t->left;
-        else if ( key  >  t->key )‏
-            t = t->right;
-        else // found it!
-            return t;
-    }
-    return nullptr;
-}
+//
+//TreeNode::TreeNode* newNode(string k, int e,TreeNode * l, TreeNode * r)
+//{
+//    ++nodesCreated;
+//    return new TreeNode(k, e, l, r);
+//}
+//
+//TreeNode::insert(string key, int info, TreeNode *RootT ){
+//    if (RootT != NULL)
+//        RootT = newNode(key, info, nullptr, nullptr);
+//    TreeNode* t = RootT;
+//    while (t->key !=  key){
+//        if (key < t->key){
+//            if (t->left != NULL)
+//                t->left = newNode(key, info, NULL, NULL);
+//            t = t->left;
+//        }else if(key > t->key){
+//            if (t->right != NULL)
+//                t->right = newNode(key, info, NULL, NULL);
+//            t = t->right;
+//        }
+//    }
+//    t->info = info;
+//    return RootT;
+//}
 
-template <typename KeyType, typename ElementType, typename TreeNode>
-TreeNode* remove( KeyType key, TreeNode * t )‏
+//TreeNode::find(string key, TreeNode* t){
+//    while (t != nullptr){
+//        if ( key  <  t->key )
+//            t = t->left;
+//        else if ( key  >  t->key )
+//            t = t->right;
+//        else // found it!
+//            return t;
+//    }
+//    return nullptr;
+//}
+//
+//TreeNode::remove( string key, TreeNode* t )
+//{
+//    TreeNode* toRemove = find(key, t); // must do in-line, assn
+//    if (toRemove -> left  ==  nullptr){
+//        if ( toRemove -> right  ==  nullptr )
+//            return NULL;
+//        else
+//            return toRemove -> right;
+//    }else if( toRemove -> right == nullptr ){
+//            return toRemove->left;
+//    }else{  // hard case, two children
+//        TreeNode * predecessorNode = findPred(key, toRemove);
+//        swapKeyAndInfo(predecessorNode, toRemove);
+//        toRemove = remove(key, toRemove);
+//        return toRemove;
+//    }
+//}
+
+void TreeNode::print(ostream & out, TreeNode* t)
 {
-    TreeNode* toRemove = find(key, t); // must do in-line, assn
-    if (toRemove -> left  ==  nullptr){‏
-        if ( toRemove -> right  ==  nullptr )‏
-            return NULL;
-        else
-            return toRemove -> right;
-    }else if( toRemove -> right == nullptr )‏{
-            return toRemove->left;
-    }else{  // hard case, two children
-        TreeNode * predecessorNode = findPred(key, toRemove);
-        swapKeyAndInfo(predecessorNode, toRemove);
-        toRemove = remove(key, toRemove);
-        return toRemove;
-    }
-}
-
-template <typename KeyType, typename ElementType, typename TreeNode>
-void print(ostream & out, TreeNode* t)‏
-{
-    if ( t )‏ {
-        out << ‘[‘;
+    if ( t ){
+        out << '[';
                  print(out, t->left);
                  out << '(' << t->key << ',' << t->info << ')';
                  print(out, t->right);
-                 out << ‘]‘;
+                 out << ']';
     }else
         out << "nullptr";
 }
 
-template <typename KeyType, typename ElementType, typename TreeNode>
-static void deleteNode(TreeNode * t){
+static void deleteNode(TreeNode* t){
     --nodesCreated;
     delete t;
 }
         
 
-template <typename KeyType, typename ElementType, typename TreeNode>
-static void deleteTree(TreeNode * t)‏{
-    if ( t )‏{
+static void deleteTree(TreeNode* t){
+    if ( t ){
         deleteTree(t->left);
         deleteTree(t->right);
         deleteNode(t);
     }
 }
 
-template <typename KeyType, typename ElementType, typename TreeNode>
 class BinarySearchTree{
     TreeNode * root;
 public:
-    void insert(KeyType key, ElementType info);
-    ElementType find(KeyType key);
-    void remove(KeyType key);
+    void insert(string key, int info);
+    int find(string key);
+    void remove(string key);
     void print(ostream & out);
 };
 
-template <typename KeyType, typename ElementType, typename TreeNode>
-void insert(KeyType key, ElementType info)‏{
+void BinarySearchTree::insert(string key, int info){
     root = TreeNode::insert(key, info, root);
 }
         
-template <typename KeyType, typename ElementType, typename TreeNode>
-ElementType find(KeyType key)‏{
-    TreeNode * t = TreeNode::find(key, root);
+int BinarySearchTree::find(string key){
+    TreeNode* t = TreeNode::find(key, root);
     if ( !t ){
-        insert(key, ElementType());
+        insert(key, int());
         t = TreeNode::find(key, root);
     }
     return t->info;
 }
 
-template <typename KeyType, typename ElementType, typename TreeNode>
-void remove(KeyType key)‏{
+void BinarySearchTree::remove(string key){
     root = TreeNode::remove(key, root);
 }
         
-template <typename KeyType, typename ElementType, typename TreeNode>
-void print(ostream & out)‏{
+void BinarySearchTree::print(ostream& out){
     TreeNode::print(out, root);
 }
 
         
-void insertAllWords(BinarySearchTree & T, int partition, char *fileName){
+void insertAllWords(BinarySearchTree& T, int partition, char *fileName){
     Timer t;
     double eTime;
     ifstream f(input_file.c_str());
