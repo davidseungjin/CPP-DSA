73a8aabf5594a120ad62050f949492d18b949c4c
davidseungjin <ryonlee@gmail.com>
2019-05-19 22:44:36 -0700

    hw7

--- hw7/main.cpp
+++ hw7/main.cpp
@@ -1,97 +1,83 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include "Timer.h"
 
 using namespace std;
 
 
 class Sorting{
-
+    string* arr;
+    int index;
+    int low;
+    int high;
 public:
-    Sorting();
-//    Sorting(int maxsize)
-//        : arr(new string[maxsize]), index(0), low(0), high(maxsize){
-//            cout << "Sorting constructor called" << endl;
-//    }
+    Sorting(int maxsize)
+        : arr(new string[maxsize]), index(0), low(0), high(maxsize){
+            cout << "Sorting constructor called" << endl;
+    }
 
-//    insertAllFromFile(int partition, char *fileName);        // should be implemented in this
-
+    virtual void push(string w) { arr[index++] = w; }
+    void insertAllFromFile(int partition, string file_name){
+        Timer t;
+        double eTime;
+        
+        ifstream f(file_name.c_str());
+        t.start();
+        string w;
+        if(f.is_open()){
+            for(int i = 0; i< partition*10; i++){
+                getline(f, w);
+                push(w);
+            }
+        }
+        t.elapsedUserTime(eTime);
+        f.close();
+    }
     
-    //    void print(ostream & out);                               // should be implemented in this
-//    virtual void sort()=0;           // it is pure virtual. should not be implemented in this
-    virtual void sort(string arr[], int low, int high)=0;
-    virtual void push(string w)=0;
-    virtual void print_test()=0;
+
+    void sort(int low, int high){};
+    
+    void print(){      // should be implemented in this
+        for(int i = 0 ; i < high ; i++)
+            cout << arr[i] << endl;
+    }
     ~Sorting(){
         cout << "Sorting destructor called" << endl;
     }
 };
 
 
-class Insertionsort:public Sorting{
-    string* arr;
-    int index;
-    int capacity;
-public:
-    virtual void push(string w) { arr[index++] = w; }
-    Insertionsort(int maxsize)
-    : arr(new string[maxsize]), index(0), capacity(maxsize){
-        cout << "Insertionsort constructor called" << endl;
-    }
-    virtual void sort(){
-        int i, j;
-        string key;
-        
-        for (i = 1; i < capacity; i++)
-        {
-            key = arr[i];
-            j = i - 1;
-            
-            while (j >= 0 && arr[j] > key)
-            {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
-    }
-    
-    void printArray(string arr[], int capacity);
-    virtual void print_test(){
-        for(int i=0; i<capacity; i++)
-            cout << arr[i] << endl;
-    }
-    ~Insertionsort(){
-        cout << "Insertionsort destructor called" << endl;
-    }
-};
-
-void makearray(string file_name, Sorting& L){
-    ifstream f(file_name.c_str());
-    string w;
-    if(f.is_open()){
-        while(getline(f, w)){
-            L.push(w);
-        }
-        f.close();
+void measureAll(string file_name)
+{
+    for (int i=1; i<=3; ++i)
+    {
+        cout << " ========= " << "Partition" << i << " ========= " << endl;
+        cout << "insertion sort" << endl;
+        T.insertAllFromFile(i, file_name);         // insertion sort
+//        cout << "\nquick sort" << endl;
+//        b.insertAllFromFile(i, file_name);         // Quick sort
+//        cout << "\nheap sort" << endl;
+//        c.insertAllFromFile(i, file_name);         // Heap sort
     }
 }
+
+int main(int argc, char* argv[])
+{
+    const char* input_file = argc == 2 ? argv[1]: "random_5.txt";
     
+    Sorting s(6);
+
+    Sorting& T = s;
     
-int main(){
-    const char* input_file = "random_5.txt";
+    T.insertAllFromFile(int partition, input_file);
+    T.print();
 
-    Insertionsort A(6);
-
-    Sorting& s = A;
-    makearray(input_file, s);
-    s.print_test();
-
-    s.sort();
-    s.print_test();
-
-  
     
+    T.sort(0, 6);
+    T.print();
+
+    measureAll(input_file);
     
     return 0;
 
